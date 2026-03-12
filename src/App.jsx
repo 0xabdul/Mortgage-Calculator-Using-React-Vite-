be5db8319160962529778ae7c786b5bf657db4d3
C H A R A N <abdullahmuhammad6005@gmail.com>
2026-03-12 13:20:25 +0530

    Fix image paths in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -82,7 +82,7 @@ function App() {
       </div>
       <br></br>
       <div className='cal-btn' onClick={late}>
-        <img src='src/assets/icon-calculator.svg' alt='calculator-img'></img>
+        <img src='assets/icon-calculator.svg' alt='calculator-img'></img>
         <button>Calculate Repayments</button>
       </div>
       </div>
@@ -90,7 +90,7 @@ function App() {
         {co &&(
         <div className='sec-cont'>
         <div className='imgs'>
-          <img src='src/assets/illustration-empty.svg' alt='empty-img'></img>
+          <img src='assets/illustration-empty.svg' alt='empty-img'></img>
         </div>
         <div className='name-result'>
           <h2>Result Show Here</h2>
